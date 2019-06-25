06bf7393209b0e333e28d0942e868ef9171e192b
killing47 <whipple.box47@gmail.com>
2019-06-25 18:02:21 +0900

    docsを変更

--- app/Http/Controllers/User/DailyReportController.php
+++ app/Http/Controllers/User/DailyReportController.php
@@ -20,7 +20,7 @@ class DailyReportController extends Controller
 
     /**
      * Display a listing of the resource.
-     *
+     * Display a listing of search resources.
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
