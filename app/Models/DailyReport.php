d5808917db9361b3c0a74d4eb8cdbdaf9bcf92a9
killing47 <whipple.box47@gmail.com>
2019-06-25 17:52:39 +0900

    docsを変更

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class DailyReport extends Model
 {
     use SoftDeletes;
+
     protected $dates = ['reporting_time','deleted_at'];
 
     protected $fillable = [
