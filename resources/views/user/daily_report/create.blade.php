a7f11a75a70bdbf95612c9601de62cc72ab7ff5d
killing47 <whipple.box47@gmail.com>
2019-06-26 11:48:05 +0900

    old()修正

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -10,11 +10,11 @@
         <span class="help-block"></span>
       </div>
       <div class="form-group?{{ $errors->has('title') ? ' has-error' : '' }}">
-        {!! Form::input('text', 'title', old('title'), ['autofocus', 'class' => 'form-control', 'placeholder' => 'Title']) !!}
+        {!! Form::input('text', 'title', null, ['autofocus', 'class' => 'form-control', 'placeholder' => 'Title']) !!}
         <span class=" help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group {{ $errors->has('content') ? ' has-error' : '' }}">
-        {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10']) !!}
+        {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10']) !!}
         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
       {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
