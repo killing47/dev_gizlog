379f99fbde5551477f974600427f59b89370bc4c
killing47 <whipple.box47@gmail.com>
2019-07-11 18:10:15 +0900

    Commentコントローラ変更

--- app/Http/Controllers/User/CommentController.php
+++ app/Http/Controllers/User/CommentController.php
@@ -19,26 +19,6 @@ class CommentController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return redirect()->route('question.index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  App\Http\Requests\User\CommentRequest  $request
@@ -50,50 +30,5 @@ class CommentController extends Controller
         $input['user_id'] = Auth::id();
         $this->comment->fill($input)->save();
         return redirect()->route('question.show', ['id' => $input['question_id']]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::group(['prefix' => '/', 'user.', 'namespace' => 'User'], function () {
 
     Route::resource('report', DailyReportController::class);
     Route::resource('question', QuestionController::class);
-    Route::resource('comment', CommentController::class);
+    Route::resource('comment', CommentController::class, ['only' => 'store']);
 });
 
 
